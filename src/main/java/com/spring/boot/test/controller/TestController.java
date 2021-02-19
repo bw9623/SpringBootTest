74c8bb131d8691c1f8d3dad9fabb41c078f7f5f4
bw9623 <bw9623@naver.com>
2021-02-20 04:27:20 +0900

    thymeleaf

--- src/main/java/com/spring/boot/test/controller/TestController.java
+++ src/main/java/com/spring/boot/test/controller/TestController.java
@@ -4,15 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.boot.test.model.vo.TestVo;
 
 @Controller
 public class TestController {
 	
 	@RequestMapping(value = "/home")
 	public String home() {
+		
 		return "index.html";
 	}
 	
@@ -20,10 +24,13 @@ public class TestController {
 	@RequestMapping(value = "/valueTest")
 	public String valueTest() {
 		String value = "테스트 String";
+		
 		return value;
 	}
 	
-	@RequestMapping("/test")
+	
+	// JSP
+	@RequestMapping(value = "/test")
 	public ModelAndView test() throws Exception {
 		ModelAndView mav = new ModelAndView("test");
 		mav.addObject("name", "민병욱");
@@ -34,6 +41,17 @@ public class TestController {
 		testList.add("c");
 		
 		mav.addObject("list", testList);
+		
 		return mav;
 	}
+	
+	// thymeleaf
+	@RequestMapping(value = "/thymeleafTest")
+	public String thymeleafTest(Model model) {
+		TestVo testModel = new TestVo("mean", "민병욱");
+		model.addAttribute("testModel", testModel);
+		
+		return "thymeleaf/thymeleafTest";
+	}
+	
 }
