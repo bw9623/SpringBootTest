01691e1284266f4a0d8b7f0178f1b5a6825c0e36
bw9623 <bw9623@naver.com>
2021-02-20 20:25:05 +0900

    login view

--- src/main/java/com/spring/boot/controller/UserController.java
+++ src/main/java/com/spring/boot/controller/UserController.java
@@ -1,16 +1,24 @@
 package com.spring.boot.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/user")
 public class UserController {
 	
 	
-	@RequestMapping(value = "/signin")
-	public String login() {
-		return "thymeleaf/common/main";
+	@RequestMapping(value = "/signin", method = RequestMethod.GET)
+	public String signinView() throws Exception {
+		return "user/signin";
+	}
+	
+	/**
+	 * 로그인 페이지 매핑
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String LoginView() {
+		return "thymeleaf/user/login";
 	}
 }
