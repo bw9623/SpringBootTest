792341bff182d0b922f1f19e50043ecd4bbd855d
bw9623 <bw9623@naver.com>
2021-02-21 23:04:55 +0900

    게시판 페이징, 검색 추가

--- src/main/java/com/spring/boot/controller/BoardController.java
+++ src/main/java/com/spring/boot/controller/BoardController.java
@@ -1,14 +1,14 @@
 package com.spring.boot.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,8 +30,17 @@ public class BoardController {
 	
 	
 	@GetMapping("/list")
-	public String list(Model model) {
-		List<Board> boards = boardRepository.findAll();
+	public String list(Model model, @PageableDefault(size = 15) Pageable pageable,
+			  		   @RequestParam(required = false, defaultValue = "") String searchText) { 
+//		Page<Board> boards = boardRepository.findAll(pageable);	// PageRequest.of(0, 20) 0 : page, 2 : size(한 페이지에 보여줄 게시글 개수)
+		Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+		// boards.getPageable();		 페이지 정보
+		// boards.getTotalElements();	 전체 건수
+		// boards.getTotalPages();		 총 몇개의 페이지가 있는지?
+		int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+		int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
 		model.addAttribute("boards", boards);
 		return "board/list";
 	}
--- src/main/java/com/spring/boot/repository/BoardRepository.java
+++ src/main/java/com/spring/boot/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package com.spring.boot.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.spring.boot.vo.Board;
@@ -10,4 +12,7 @@ public interface BoardRepository extends JpaRepository<Board, Long>{
 	
 	List<Board> findByTitle(String title);
 	List<Board> findByTitleOrContent(String title, String Content); // And 도 가능
+	
+	// 게시글 검색
+	Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);	
 }
